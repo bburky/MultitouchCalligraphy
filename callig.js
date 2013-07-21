b2e499ffd560f5a406e00b6e813feaca5a3ec84e
Blake Burkhart <bburky@bburky.com>
2013-07-21 14:25:51 -0400

    Working panning, mostly

--- callig.js
+++ callig.js
@@ -1,9 +1,13 @@
 var container = document.getElementById('canvas-container');
-var size = Math.max(window.innerWidth, window.innerHeight);
+var size = Math.min(window.innerWidth, window.innerHeight);
 var paper = Raphael(container, size, size);
 
 var unpaired;
 var currentstrokes = {};
+var panning;
+
+var xOffset = 0;
+var yOffset = 0;
 
 function touchstart (event) {
   event.preventDefault();
@@ -11,30 +15,32 @@ function touchstart (event) {
 
   for (var i=0; i<touches.length; i++) {
     var id = touches[i].identifier;
+    var x = xOffset + touches[i].pageX;
+    var y = yOffset + touches[i].pageY;
 
     if (currentstrokes[id] !== undefined) {
       console.warn('start: id already in use:' + id);
     } else if (unpaired === undefined) {
-      console.log('unpaired start: ' + id + ' x:' + touches[i].pageX + ' y:' + touches[i].pageY);
+      console.log('unpaired start: ' + id + ' x:' + x + ' y:' + y);
 
       stroke = {
         color: '#' + Math.floor(Math.random() * 0xffffff).toString(16),
         points: {},
-        path: paper.path(['M', touches[i].pageX, touches[i].pageY].join(' '))
+        path: paper.path(['M', x, y].join(' '))
       };
       unpaired = stroke;
       currentstrokes[id] = stroke;
 
-      stroke.points[id] = [ [touches[i].pageX, touches[i].pageY] ];
+      stroke.points[id] = [ [x, y] ];
       stroke.path.attr('fill', stroke.color);
     } else {
-      console.log('paired start: ' + id + ' x:' + touches[i].pageX + ' y:' + touches[i].pageY);
+      console.log('paired start: ' + id + ' x:' + x + ' y:' + y);
 
       stroke = unpaired;
       unpaired = undefined;
       currentstrokes[id] = stroke;
 
-      stroke.points[id] = [ [touches[i].pageX, touches[i].pageY] ];
+      stroke.points[id] = [ [x, y] ];
       stroke.path.attr('path', generatePathData(stroke));
     }
   }
@@ -46,23 +52,51 @@ function touchmove (event) {
 
   for (var i=0; i<touches.length; i++) {
     var id = touches[i].identifier;
+    var x = xOffset + touches[i].pageX;
+    var y = yOffset + touches[i].pageY;
 
-    if (currentstrokes[id] === undefined) {
+     if (panning !== undefined && panning.id === id) {
+      console.log('panning move: ' + id + ' x:' + x + ' y:' + y);
+
+      xOffset += panning.x - x;
+      yOffset += panning.y - y;
+      paper.setViewBox(xOffset, yOffset, size, size, false);
+
+      panning.x = x;
+      panning.y = y;
+    } else if (currentstrokes[id] === undefined) {
       console.warn('move: id already ended:' + id);
-    } else if (currentstrokes[id] === unpaired) {
-      console.log('unpaired move: ' + id + ' x:' + touches[i].pageX + ' y:' + touches[i].pageY);
+      console.log(panning);
+    } else if (currentstrokes[id] === unpaired && panning === undefined) {
+      console.log('panning start: ' + id + ' x:' + x + ' y:' + y);
 
-      // Kill the unpaired stroke if it is moving
+      // Kill the unpaired stroke, remove from currentstrokes
+      stroke = currentstrokes[id];
       unpaired = undefined;
       delete currentstrokes[id];
 
-      // TODO: pan the canvas when this happens
+      xOffset += stroke.points[id][0][0] - x;
+      yOffset += stroke.points[id][0][1] - y;
+      paper.setViewBox(xOffset, yOffset, size, size, false);
+
+      panning = {
+        id: id,
+        x: x,
+        y: y
+      };
+    } else if(currentstrokes[id] === unpaired) {
+      console.log('unpaired move: ' + id + ' x:' + x + ' y:' + y);
+
+      // Kill the unpaired stroke, remove from currentstrokes
+      stroke = currentstrokes[id];
+      unpaired = undefined;
+      delete currentstrokes[id];
     } else {
-      console.log('current move: ' + id + ' x:' + touches[i].pageX + ' y:' + touches[i].pageY);
+      console.log('current move: ' + id + ' x:' + x + ' y:' + y);
 
       stroke = currentstrokes[id];
 
-      stroke.points[id].push([ [touches[i].pageX, touches[i].pageY] ]);
+      stroke.points[id].push([ [x, y] ]);
       stroke.path.attr('path', generatePathData(stroke));
     }
   }
@@ -74,19 +108,24 @@ function touchend (argument) {
 
   for (var i=0; i<touches.length; i++) {
     var id = touches[i].identifier;
+    var x = xOffset + touches[i].pageX;
+    var y = yOffset + touches[i].pageY;
 
-    if (currentstrokes[id] === undefined) {
+    if (panning !== undefined && panning.id === id) {
+      console.log('panning end: ' + id + ' x:' + x + ' y:' + y);
+      panning = undefined;
+    } else if (currentstrokes[id] === undefined) {
       console.warn('end: id already ended:' + id);
     } else if (currentstrokes[id] === unpaired) {
-      console.log('unpaired end: ' + id + ' x:' + touches[i].pageX + ' y:' + touches[i].pageY);
+      console.log('unpaired end: ' + id + ' x:' + x + ' y:' + y);
 
-      // Kill the unpaired stroke if it ends
+      // Kill the unpaired stroke and remove from currentstrokes
       unpaired = undefined;
       delete currentstrokes[id];
     } else {
-      console.log('current end: ' + id + ' x:' + touches[i].pageX + ' y:' + touches[i].pageY);
+      console.log('current end: ' + id + ' x:' + x + ' y:' + y);
 
-      // Delete the current stroke for the id. Note that the stroke may remain
+      // Delete the currentstroke for the id. Note that the stroke may remain
       // in currentstrokes if the other touch has not yet ended
       delete currentstrokes[id];
     }
